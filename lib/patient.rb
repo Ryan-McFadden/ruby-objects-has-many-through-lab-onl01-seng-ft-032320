1baa8699b7aaf6683878a72d64b740455d135dcf
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-08 06:23:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,6 +2,17 @@ class Patient
   
   @@all = []
   
+  def intialize(name)
+    @name = name
+    @@all << self
+  end
   
+  def new_appointment(doctor, date)
+    Appointment.new()
+  end
+  
+  def self.all
+    @@all 
+  end
   
 end
